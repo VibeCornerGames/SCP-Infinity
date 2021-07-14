360329eb7747c6ddcf89aeff4e283b152ca815d5
jonesy-b-dev <jonasdebruin05@gmail.com>
2021-07-14 16:07:12 +0200

    Package Fixes
    Fixed a ton of packaging warnings and errors but still its not packaging

--- Game/betterbeworkin/Intermediate/Build/Win64/UE4Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.29.30037
-#include "D:/Code Projects/UE/Flux Entertainment/SCP-Infinity-Github3/Game/betterbeworkin/Intermediate/Build/Win64/UE4Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
