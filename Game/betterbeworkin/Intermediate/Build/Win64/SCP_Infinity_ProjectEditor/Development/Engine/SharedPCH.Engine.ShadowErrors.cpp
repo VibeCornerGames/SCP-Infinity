dacb7c33ed3bb58ccb21942837a9ed7560f08f4c
Jonas de Bruin <59418915+jonesy-b-dev@users.noreply.github.com>
2022-01-27 10:53:46 +0100

    New Working project
    * Made a new project and copied all the content in it
    Works again and this push better work

--- Game/betterbeworkin/Intermediate/Build/Win64/SCP_Infinity_ProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++ Game/betterbeworkin/Intermediate/Build/Win64/SCP_Infinity_ProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -1,2 +1,2 @@
-// Compiler: 14.30.30705
-#include "D:/Code Projects/UE/Flux Entertainment/SCP-Infinity-Github3/Game/betterbeworkin/Intermediate/Build/Win64/SCP_Infinity_ProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
+// Compiler: 14.30.30709
+#include "D:/Code Projects/UE/SCP-Infinity/Game/betterbeworkin/Intermediate/Build/Win64/SCP_Infinity_ProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ Game/betterbeworkin/Intermediate/Build/Win64/SCP_Infinity_ProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp.old
@@ -0,0 +1,2 @@
+// Compiler: 14.30.30705
+#include "D:/Code Projects/UE/Flux Entertainment/SCP-Infinity-Github3/Game/betterbeworkin/Intermediate/Build/Win64/SCP_Infinity_ProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
